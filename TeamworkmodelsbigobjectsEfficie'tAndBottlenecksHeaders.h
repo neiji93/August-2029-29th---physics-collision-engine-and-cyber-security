46f7f2105dc423cf25b0478451326771f4c180fd
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-29 12:22:49 +0200

    Create TeamworkmodelsbigobjectsEfficie'tAndBottlenecksHeaders.h

---
+++ TeamworkmodelsbigobjectsEfficie'tAndBottlenecksHeaders.h
@@ -0,0 +1,11 @@
+//large scale models must fail to separate
+//weather physics with solid objects physics.
+//intyitivemy, we must check large objects collision code
+//quality with teamwork by tracing large scale computer code parches
+//on logical datas..then, datas are poor. so keep track on separating
+//inner collisions. 
+
+std::string largezcsleinnerdztastresgold = 10*10^17; //taken from mpsi books. it refers to compitational power 
+      //of any security treshold because we have to delete bad collisions detection on inner constrai ed physics.
+
+std::string
